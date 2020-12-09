00212192e654d5d3c2cce80a767ce9826504ba9e
federica <fdelgadodeleon@linti.unlp.edu.ar>
2020-12-09 11:46:52 -0300

    Listado de empleados

--- src/modules/employees/Employees.jsx
+++ src/modules/employees/Employees.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Snackbar, SnackbarContent, Typography, Button } from '@material-ui/core';
+import EmployeeTable from './EmployeeTable';
+import './employees.css';
+import axios from 'axios';
 
-const Employees = () => {
+const Employees = ({ history }) => {
+  const [employees, setEmployees] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+
+  useEffect(() => {
+    setLoading(true)
+    axios('https://5faeb24463e40a0016d8a044.mockapi.io/api/employees')
+      .then(res => {
+        setEmployees(res.data)
+        setLoading(false)
+      })
+      .catch(error => {
+        console.log(error)
+        setLoading(false)
+        setError("Hubo un error en la consulta")
+      })
+  }, [])
+
+  const goToForm = () => {
+    history.push('/employees/new')
+  }
+
   return (
-    <div>Empleados</div>
+    <div>
+      <div className="button">
+        <Button variant="outlined" color="secondary" onClick={goToForm}>Agregar</Button>
+      </div>
+      <Typography variant="h4" className="center">Empleados</Typography>
+      <EmployeeTable employees={employees} loading={loading} />
+      {error && (
+        <Snackbar open={!!error} onClose={() => setError(null)}>
+          <SnackbarContent className="error" message={error} />
+        </Snackbar>
+      )}
+    </div>
   )
 }
 
